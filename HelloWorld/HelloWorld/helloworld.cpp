b34c5611a66024acfd0956ff686681cd78588673
YSJ <sungzzuu@gmail.com>
2022-09-13 19:03:30 +0900

    0913 강의

--- HelloWorld/HelloWorld/helloworld.cpp
+++ HelloWorld/HelloWorld/helloworld.cpp
@@ -7,49 +7,38 @@
 using namespace std;
 using namespace chrono;
 
-int sum;
+volatile int sum[8*8];
 constexpr int LOOP = 50000000;
 mutex mylock;
 
-void worker(int num_thread)
+void worker(int thread_id, int num_thread)
 {
-	volatile int local_sum = 0;
-	for (auto i = 0; i < LOOP/ num_thread; i++)
-	{
-		//mylock.lock();
-		//sum = sum + 1; // DATA RACE 일어남.
-		//sum += 1; // data race 일어나지 않음.
-		local_sum += 2;
-		//mylock.unlock();
-	}
-	mylock.lock();
-	sum += local_sum;
-	mylock.unlock();
+	for (auto i = 0; i < LOOP / num_thread; i++)
+		sum[thread_id*8] += 2;
 }
 
 int main()
 {
-
 	for (int num_thread = 1; num_thread <= 8; num_thread*=2)
 	{
-		sum = 0;
+		for (auto& a : sum) a = 0;
+
 		vector<thread> threads;
 		auto t = high_resolution_clock::now();
 
 		for (int i = 0; i < num_thread; ++i)
 		{
-			threads.emplace_back(worker, num_thread);
+			threads.emplace_back(worker, i, num_thread);
 		}
 		for (auto& th : threads)
 			th.join();
 		auto d = high_resolution_clock::now() - t;
 		cout << duration_cast<milliseconds>(d).count() << " msecs ";
-		// 580만 근처의 값들
-		cout << "Threads "<< num_thread <<" Sum = " << sum << '\n';
+		int f_sum = 0;
+		for (auto& a : sum) f_sum += a;
+
+		cout << "Threads "<< num_thread <<" Sum = " << f_sum << '\n';
 
 	}
-
-
-
 }
  
