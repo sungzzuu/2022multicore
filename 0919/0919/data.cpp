f9270938809c74b8815c90546d0ead9b786b21c8
YSJ <sungzzuu@gmail.com>
2022-09-20 17:26:58 +0900

    피터슨 알고리즘 성능 테스트

--- 0919/0919/data.cpp
+++ 0919/0919/data.cpp
@@ -1,31 +1,58 @@
 #include <iostream>
 #include <thread>
 #include <mutex>
+#include <chrono>
 using namespace std;
+using namespace chrono;
 
-volatile int g_data = 0;
-volatile bool g_ready = false;
+volatile int sum = 0;
+volatile int victim = 0;
 
-void reicever()
+volatile bool flag[2];
+
+mutex l;
+void p_unlock(int _id)
 {
-	while (false == g_ready); // loop를 도는 동안 lock이 풀리지 않음
-	cout << "Data = " << g_data << endl;
+	flag[_id] = false;
 }
 
-void sender()
+void p_lock(int _id)
 {
-	cout << "Enter Data: ";
-	int a;
-	cin >> a;
-	g_data = a;
-	g_ready = true;
+	int o = 1 - _id;
+	flag[_id] = true;
+	victim = _id;
+	while (flag[o] && victim == _id)
+	{
+
+	}
+}
+
+void worker(int th_id)
+{
+	for (int i = 0; i < 25000000; i++)
+	{
+		//p_lock(th_id);
+		l.lock();
+		sum = sum + 2;
+		l.unlock();
+		//p_unlock(th_id);
+
+		// 피터슨이 성능도 별로고 data race도 일어남.
+		// 이유는??
+
+	}
 }
 
 int main()
 {
-	thread r{ reicever };
-	thread s{ sender };
+	auto start_t = high_resolution_clock::now();
+	thread t1{ worker, 0 };
+	thread t2{ worker, 1 };
+	t1.join();
+	t2.join();
+	auto end_t = high_resolution_clock::now();
+	auto exec_t = end_t - start_t;
+	int exec_ms = duration_cast<milliseconds>(exec_t).count();
 
-	r.join();
-	s.join();
+	cout << "Result = " << sum << " Exec Time: " << exec_ms << "ms" << endl;
 }
