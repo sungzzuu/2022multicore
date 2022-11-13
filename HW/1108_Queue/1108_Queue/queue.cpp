3866afa269264a7cc3829f6a79d77c898c6e25bb
YSJ <sungzzuu@gmail.com>
2022-11-13 16:46:57 +0900

    11/8 수업까지 완료

--- HW/1108_Queue/1108_Queue/queue.cpp
+++ HW/1108_Queue/1108_Queue/queue.cpp
@@ -34,7 +34,7 @@ public:
 
 class C_QUEUE {
 	NODE *head, *tail;
-	mutex ll;
+	null_mutex ll;
 public:
 	C_QUEUE()
 	{
@@ -61,6 +61,72 @@ public:
 		head = head->next;
 		delete p;
 		ll.unlock();
+		return;
+	}
+	void print20()
+	{
+		NODE* p = head->next;
+		for (int i = 0; i < 20; ++i) {
+			if (p == nullptr) break;
+			cout << p->v << ", ";
+			p = p->next;
+		}
+		cout << endl;
+	}
+
+	void clear()
+	{
+		while (head->next != nullptr) {
+			NODE* t = head;
+			head = head->next;
+			delete t;
+		}
+		tail = head;
+	}
+};
+class LF_QUEUE {
+	NODE* head, * tail;
+public:
+	LF_QUEUE()
+	{
+		head = tail = new NODE{ -1 };
+	}
+	bool CAS(NODE*volatile* next, NODE* old_p, NODE* new_p)
+	{
+		atomic_compare_exchange_strong(
+			reinterpret_cast<volatile atomic_llong*>(next),
+			reinterpret_cast<long long*>(&old_p),
+			reinterpret_cast<long long>(new_p));
+	}
+	void ENQ(int x)
+	{
+		NODE* p = new NODE(x);
+		while (true) {
+			NODE* last = tail;
+			NODE* next = last->next;
+			if (last != tail) continue;
+			if (nullptr == next) {
+				if (CAS(&(last->next), nullptr, p)) {
+					CAS(&tail, last, p);
+					return;
+				}
+			}
+			else CAS(&tail, last, next);
+		}
+		tail->next = p;
+		tail = p;
+	}
+	int DEQ()
+	{
+		/*NODE* p = head;
+		if (p->next == nullptr)
+		{
+			return -1;
+		}
+		int res = p->next->v;
+		head = head->next;
+		delete p;
+		return res;*/
 	}
 	void print20()
 	{
